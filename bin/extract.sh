7ec3c6d6a886c8ba54bad83a11dfffe8cc3142b2
Andy <andy@andydoestech.com>
2020-12-08 10:46:11 -0500

    updated scripts

---
+++ bin/extract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+stripxml() {
+	sed 's/\(<\/[^>]\+>\)/\1\n/g;s/<[^>]\+>//g'
+}
+
+getcdata() {
+	perl -0777 -pe 's/^.*<!\[CDATA\[(.*)\]\]>.*$/\1/igs'
+}
+
+BASE="$(dirname $0)/.."
+ARTBASE="$BASE/artifacts"
+
+[[ -d $ARTBASE ]] || mkdir $ARTBASE
+[[ -d $ARTBASE/commands ]] || mkdir $ARTBASE/commands
+[[ -d $ARTBASE/scripts ]] || mkdir $ARTBASE/scripts
+
+xmllint --xpath  /unload/x_293397_web_cli_command_scripts/name $BASE/x_293397_web_cli_command_scripts.xml | stripxml | while read line; do
+	xmllint --xpath '/unload/x_293397_web_cli_command_scripts[name="'"$line"'"]/script' $BASE/x_293397_web_cli_command_scripts.xml | stripxml > $ARTBASE/scripts/$line.script
+done
+
+xmllint --xpath '/unload/x_293397_web_cli_code_entries/name' $BASE/x_293397_web_cli_code_entries.xml | stripxml | while read line; do
+	xmllint --xpath '/unload/x_293397_web_cli_code_entries[name="'"$line"'"]/script' $BASE/x_293397_web_cli_code_entries.xml | getcdata > $ARTBASE/commands/$line.js
+done
